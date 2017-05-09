1e8e139c5f25f512694da8d68f7b4bdb0fddefdb
Prateek <Prateek>
2017-05-09 19:54:42 +0530

    First commit

---
+++ hospital/hospital-ejb/build/generated-sources/ap-source-output/beans/userEntity_.java
@@ -0,0 +1,13 @@
+package beans;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-09T12:15:51")
+@StaticMetamodel(userEntity.class)
+public class userEntity_ { 
+
+    public static volatile SingularAttribute<userEntity, Long> id;
+
+}
